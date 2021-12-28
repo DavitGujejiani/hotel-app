eb451f60a9d9cc7a4586b70e5a02c0f10cb0a178
Davit <davit.gujejiani@iliauni.edu.ge>
2021-12-29 01:17:56 +0400

    useForm error validation works

--- src/modules/contact/components/ContactForm.js
+++ src/modules/contact/components/ContactForm.js
@@ -1,63 +1,92 @@
 import React from 'react';
+import {useForm} from 'react-hook-form';
 
 function ContactForm() {
+    const {register, handleSubmit, formState: { errors }} = useForm();
+
+    const formSubmitHandler = (data) => {
+        console.log(data);
+    }
+
     return (
-        <form className="w-full border rounded-lg p-8 my-10">
+        <form onSubmit={handleSubmit(formSubmitHandler)} className="w-full border rounded-lg p-8 my-10">
             <div className="flex flex-wrap -mx-3 mb-6">
                 <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    <label title="This field is necessary" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                            htmlFor="grid-first-name">
-                        First Name
+                        First Name <i className="text-red-500">*</i>
                     </label>
                     <input
-                        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                        id="grid-first-name" type="text" placeholder="Jane" />
-                    <p className="text-red-500 text-xs italic">Please fill out this field.</p>
+                        {...register("firstname", {
+                            required: true,
+                        })}
+                        className={`appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white
+                         ${errors.firstname ? "border-red-500" : "border-gray-200"}`}
+                        id="grid-first-name"
+                        type="text"
+                        placeholder="Jane"/>
+                    {errors.firstname && <p className="text-red-500 text-xs italic">Please fill out this field.</p>}
                 </div>
                 <div className="w-full md:w-1/2 px-3">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    <label title="This field is necessary" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                            htmlFor="grid-last-name">
-                        Last Name
+                        Last Name <i className="text-red-500">*</i>
                     </label>
                     <input
-                        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                        id="grid-last-name" type="text" placeholder="Doe" />
+                        {...register("lastname", {
+                            required: true,
+                        })}
+                        className={`appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white
+                         ${errors.lastname ? "border-red-500" : "border-gray-200"}`}
+                        id="grid-last-name"
+                        type="text"
+                        placeholder="Doe"/>
+                    {errors.lastname && <p className="text-red-500 text-xs italic">Please fill out this field.</p>}
                 </div>
             </div>
             <div className="flex flex-wrap -mx-3 mb-6">
                 <div className="w-full px-3">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    <label title="This field is necessary" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                            htmlFor="grid-password">
-                        E-mail
+                        E-mail <i className="text-red-500">*</i>
                     </label>
                     <input
-                        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                        id="email" type="email" />
-                    <p className="text-gray-600 text-xs italic">Some tips - as long as needed</p>
+                        {...register("email", {
+                            required: true,
+                        })}
+                        className={`appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white
+                         ${errors.email ? "border-red-500" : "border-gray-200"}`}
+                        id="email"
+                        type="email"/>
+                    {/*<p className="text-gray-600 text-xs italic">Some tips - as long as needed</p>*/}
+                    {errors.email && <p className="text-red-500 text-xs italic">Please fill out this field.</p>}
                 </div>
             </div>
             <div className="flex flex-wrap -mx-3 mb-6">
                 <div className="w-full px-3">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    <label title="This field is necessary" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                            htmlFor="grid-password">
-                        Message
+                        Message <i className="text-red-500">*</i>
                     </label>
                     <textarea
-                        className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
-                        id="message" />
-                    <p className="text-gray-600 text-xs italic">Re-size can be disabled by set by resize-none /
-                        resize-y / resize-x / resize</p>
+                        {...register("message", {
+                            required: true,
+                        })}
+                        className={`no-resize appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none
+                         ${errors.message ? "border-red-500" : "border-gray-200"}`}
+                        id="message"/>
+                    {errors.message && <p className="text-red-500 text-xs italic">Please fill out this field.</p>}
                 </div>
             </div>
             <div className="md:flex md:items-center">
                 <div className="md:w-1/3">
                     <button
                         className="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-                        type="button">
+                        type="submit">
                         Send
                     </button>
                 </div>
-                <div className="md:w-2/3" />
+                <div className="md:w-2/3"/>
             </div>
         </form>
     );
