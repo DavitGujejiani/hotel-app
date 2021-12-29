c1ab71fd1bfef450a9c0136cc447a54ce191dce6
Davit <davit.gujejiani@iliauni.edu.ge>
2021-12-29 21:50:15 +0400

    minor fixes

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import ContactPage from "./modules/contact/ContactPage";
 import Header from "./Components/General/Header";
 import Slider from "./Components/General/slider/Slider";
 import Home from "./Components/modules/About/Home";
-import { Routes, Route, Link } from "react-router-dom";
+import {Routes, Route, Link} from "react-router-dom";
 
 
 function App() {
     return (
         <>
-            <Header />
-            <Slider/>
-            <ContactPage />
+            <Header/>
+            {/*<Slider/>*/}
+
             <Routes>
-              <Route path="/home" element={<Home/>} />
+                <Route path="/home" element={<Home/>}/>
+                <Route path="/contact" element={<ContactPage/>}/>
             </Routes>
-    </>
-  );
+        </>
+    );
 }
 
 export default App;
